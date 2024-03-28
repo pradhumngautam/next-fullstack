e757c22483da831046551c57aa85e731d114f1e4
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-03-28 17:53:22 +0530

    start

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+// eslint-disable-next-line react/display-name
+export default function() {
+    return <div>
+        loading...
+    </div>
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+import { Signup } from "@/components/Signup";
+
+// eslint-disable-next-line import/no-anonymous-default-export, react/display-name
+export default function () {
+    return <div>
+        <Signup/>
+    </div>
+}
